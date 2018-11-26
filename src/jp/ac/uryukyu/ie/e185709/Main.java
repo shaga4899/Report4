6750a506990c69a85dac8c6c1ca4afdf5bf48936
DakujakuReiya <dakujaku2@gmail.com>
2018-11-26 15:21:48 +0900

    step3

--- src/jp/ac/uryukyu/ie/e185709/Main.java
+++ src/jp/ac/uryukyu/ie/e185709/Main.java
@@ -3,12 +3,15 @@ package jp.ac.uryukyu.ie.e185709;
 public class Main {
     public static void main(String args[]){
 
-        try{
-            String str = null;
-            System.out.println(str.length());
-        }catch(NullPointerException e){
-            System.out.println("NullPointerExceptionが発生した");
-            System.out.println(e.getMessage());
-        }
+//        try{
+//            String str = null;
+//            System.out.println(str.length());
+//        }catch(NullPointerException e){
+//            System.out.println("NullPointerExceptionが発生した");
+//            System.out.println(e.getMessage());
+//        }
+
+        String str = "百二十三";
+        Integer.parseInt(str);
     }
 }
